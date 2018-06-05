d5b8bd93d430055c5e3d39e5a4af3be3bfb29620
Jens Reinemuth <reinemuth@jacob.de>
2018-06-05 14:52:41 +0200

    -> i15lam namespace

--- src/Workspace.php
+++ src/Workspace.php
@@ -6,7 +6,7 @@ namespace openWebX\PhpWemo;
  * Class Workspace
  * @package openWebX\PhpWemo
  */
-class Workspace extends a15lam\Workspace\Workspace
+class Workspace extends \a15lam\Workspace\Workspace
 {
     /**
      * @var string
